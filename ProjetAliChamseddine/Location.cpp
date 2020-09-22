5cea72d9c853b503efba146b1d11b5f57102b303
Ali Hassan Chamseddine <2071526@cstj.qc.ca>
2020-09-22 17:27:02 -0400

    Version Final

---
+++ ProjetAliChamseddine/Location.cpp
@@ -0,0 +1,44 @@
+// But:Écrire un programme pour calculer le montant qu’un individu devra débourser pour faire un voyage dans une
+// automobile louée.
+// Auteur: Ali Chamseddine
+// date: 2020-09-16
+
+#include <iostream>
+
+int main()
+{
+
+	setlocale(LC_ALL, "");
+	
+	// Déclaration des variables
+	int kilomètre;
+	int dureeVoyage;
+	int cout;
+
+	// Trouver le nombre de kilomètre
+	std::cout << "Combien de kilomètre à t-il parcouru";
+	std::cin >> kilomètre;
+
+	//Trouver la durée du voyage
+	std::cout << "C'est quoi la durée du voyage";
+	std::cin >> dureeVoyage;
+
+	
+
+	// Calculer le coût du voyage/ Location
+
+	//multipler le cout voyage par la durée du voyage
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+}
